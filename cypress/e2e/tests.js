0d5623dd31e4edc13106dbaafe86e9989b9816d0
jakobmadp~ <manfred.jakob@dataport.de>
2025-02-10 20:18:03 +0100

    New test cases: Register already existing user and: Validate mandatory fields in the Place-Order form

--- cypress/e2e/tests.js
+++ cypress/e2e/tests.js
@@ -1,15 +1,14 @@
 import { v4 as uuidv6 } from 'uuid';
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
-    return false
-});
+describe('Test cases for buying items from the shop', () => {
+    it('Main test case: Complete shopping process', () => {
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            // returning false here prevents Cypress from
+            // failing the test
+            return false
+        });
 
-describe('Test cases for buying items from the shop', function () {
-    it('Main test case: Complete shopping process', function () {
         cy.visit('');
-
         const username = uuidv6();
         const password = uuidv6();
 
@@ -20,7 +19,7 @@ describe('Test cases for buying items from the shop', function () {
         cy.get('#sign-password').type(password);
         cy.get('[onclick="register()"]').click();
         // check browser popup
-        cy.on('window:alert', (txt) => {
+        cy.on('window:alert', (txt) => {    // this does only work when uncaught:exception is enabled
             expect(txt).to.contains('Sign up successful');
         })
 
@@ -36,7 +35,7 @@ describe('Test cases for buying items from the shop', function () {
         cy.get('[onclick="byCat(\'notebook\')"]').click();
         cy.get('[href="prod.html?idp_=8"]:first').click();
         cy.get('[onclick="addToCart(8)"]').click();
-        cy.on('window:alert', (txt) => {
+        cy.on('window:alert', (txt) => {    // this does only work when uncaught:exception is enabled
             expect(txt).to.contains('Product added');
         })
         cy.get('.nav-item>[href="index.html"]').click();
@@ -45,7 +44,7 @@ describe('Test cases for buying items from the shop', function () {
         cy.get('[onclick="byCat(\'monitor\')"]').click();
         cy.get('[href="prod.html?idp_=14"]:first').click();
         cy.get('[onclick="addToCart(14)"]').click();
-        cy.on('window:alert', (txt) => {
+        cy.on('window:alert', (txt) => {    // this does only work when uncaught:exception is enabled
             expect(txt).to.contains('Product added');
         })
         cy.get('.nav-item>[href="index.html"]').click();
@@ -54,18 +53,18 @@ describe('Test cases for buying items from the shop', function () {
         cy.get('[onclick="byCat(\'phone\')"]').click();
         cy.get('[href="prod.html?idp_=3"]:first').click();
         cy.get('[onclick="addToCart(3)"]').click();
-        cy.on('window:alert', (txt) => {
+        cy.on('window:alert', (txt) => {    // this does only work when uncaught:exception is enabled
             expect(txt).to.contains('Product added');
         })
 
         // check cart total
         cy.get('#cartur').click();
         cy.get('#totalp').should('have.text', '1670');
-        
+
         // delete smartphone Nexus 6 and check total
         cy.contains('tr', 'Nexus 6').find('a').click();
         cy.get('#totalp').should('have.text', '1020');
-        
+
         // place order
         cy.get('.btn-success').click();
         cy.get('#orderModalLabel').should('have.text', 'Place order');
@@ -82,7 +81,8 @@ describe('Test cases for buying items from the shop', function () {
         cy.get('.sweet-alert > h2').should('have.text', 'Thank you for your purchase!');
         cy.get('.confirm').click();
     })
-    it('Send contact formular', function () {
+
+    it('Send contact formular', () => {
         cy.visit('');
         cy.get('a[data-target="#exampleModal"]').contains('Contact').click();
         cy.get('#recipient-email').type('test@abc.de', { force: true }); // during typing the input field loses the focus; workaround: force: true 
@@ -94,11 +94,53 @@ describe('Test cases for buying items from the shop', function () {
         })
     })
 
+    it('Register already existing user', () => {
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            // returning false here prevents Cypress from
+            // failing the test
+            return false
+        });
 
+        cy.visit('');
+        const username = uuidv6();
+        const password = uuidv6();
 
+        // register
+        cy.log(username);
+        cy.get('#signin2').click();
+        cy.get('#sign-username').type(username, { force: true }); // during typing the input field loses the focus; workaround: force: true 
+        cy.get('#sign-password').type(password);
+        cy.get('[onclick="register()"]').click();
+        // check browser popup
+        cy.on('window:alert', (txt) => {    // this does only work when uncaught:exception is enabled
+            expect(txt).to.contains('Sign up successful.');
+        })
 
+        // register with the same username again
+        cy.get('.nav-item>[href="index.html"]').click(); // make SignUp button clickable
+        cy.get('#signin2').click();
+        cy.get('#sign-username').clear();
+        cy.get('#sign-password').clear();
+        
+        cy.get('#sign-username').type(username, { force: true }); // during typing the input field loses the focus; workaround: force: true 
+        cy.get('#sign-password').type(password);
+        cy.get('[onclick="register()"]').click();
+        cy.on('window:alert', (abc) => {    // this does only work when uncaught:exception is enabled
+            expect(abc).to.contains('This user already exist.');
+        })
+    })
 
+    it('Validate mandatory fields in the Place-Order form', () => {
+        // Name and Creditcard must be mandatory
+        cy.visit('');
+        cy.get('#cartur').click();
+        cy.get('.btn-success').click();
+        cy.get('#orderModalLabel').should('have.text', 'Place order');
+        cy.get('[onclick="purchaseOrder()"]').click();
 
-
+        // check for not success
+        cy.on('window:alert', (txt) => {
+            expect(txt).to.contains('Please fill out Name and Creditcard.');
+        })
+    })
 })
-
